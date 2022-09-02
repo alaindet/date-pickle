3d21380c5e905bf5a630071a955bc3d52ee6da32
Alain D'Ettorre <alain.det@gmail.com>
2022-09-02 12:53:59 +0200

    Add demo file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-console.log('Date Pickle');
-
-export {};
+export * from './date-pickle';
+export * from './pickers/year-picker';
+export * from './pickers/month-picker';
+export * from './pickers/date-picker';
