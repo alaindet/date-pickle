ced35d589570d310410e58cc310bc92afbf5d2c2
Alain D'Ettorre <alain.det@gmail.com>
2022-09-06 18:38:41 +0200

    WIP

---
+++ src/utils/2d-movements.test.ts
@@ -0,0 +1,10 @@
+import { get2dMovements } from './2d-movements';
+
+describe('get2dMovements() utility function', () => {
+
+  it('should move up', () => {
+    // TODO ...
+  });
+});
+
+export {};
